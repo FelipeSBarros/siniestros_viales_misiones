35ac8c8447bf65781aa4e76efa004173e020b35c
Felipe Sodré M. Barros <felipe.b4rros@gmail.com>
2023-10-23 16:44:31 -0300

    fix code style

--- scraping_siniestros_viales/spiders/primera_edicion.py
+++ scraping_siniestros_viales/spiders/primera_edicion.py
@@ -1,19 +1,14 @@
-from pathlib import Path
-
 import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
     name = "primera_edicion"
-    start_urls = ['https://www.primeraedicion.com.ar/?s=siniestro']
+    start_urls = ["https://www.primeraedicion.com.ar/?s=siniestro"]
 
     def parse(self, response):
-        for card in response.css('article div.jeg_postblock_content'):
+        for card in response.css("article div.jeg_postblock_content"):
             yield {
-                "Titulo": card.css('h3 ::text').getall(),
-                "URL": card.css('h3 a::attr(href)').getall(),
+                "Titulo": card.css("h3 ::text").getall(),
+                "URL": card.css("h3 a::attr(href)").getall(),
                 "Fecha": card.css("div.jeg_post_meta div.jeg_meta_date a ::text").get(),
             }
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
