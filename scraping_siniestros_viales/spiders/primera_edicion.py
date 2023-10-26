f92fd1f66684d2a84f66a0b3dd3601f876f467fe
Felipe Sodré M. Barros <felipe.b4rros@gmail.com>
2023-10-25 22:25:27 -0300

    criar spider primeira edicao. raspa primera pagina de siniestro

--- scraping_siniestros_viales/spiders/primera_edicion.py
+++ scraping_siniestros_viales/spiders/primera_edicion.py
@@ -6,9 +6,27 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ["https://www.primeraedicion.com.ar/?s=siniestro"]
 
     def parse(self, response):
-        for card in response.css("article div.jeg_postblock_content"):
-            yield {
-                "Titulo": card.css("h3 ::text").getall(),
-                "URL": card.css("h3 a::attr(href)").getall(),
-                "Fecha": card.css("div.jeg_post_meta div.jeg_meta_date a ::text").get(),
-            }
+        yield from response.follow_all(
+            css="article div.jeg_postblock_content h3 a", callback=self.parse_news
+        )
+
+        pagination_links = response.css("li.next a")
+        yield from response.follow_all(pagination_links, self.parse)
+
+    def parse_news(self, response):
+        yield {
+            "Titulo": response.css("div.entry-header h1 ::text").get(),
+            "Subtitulo": response.css("div.entry-header h2 ::text").get(),
+            "URL": response.url,
+            "Fecha": response.css("div.entry-header div.jeg_meta_date a::text").get(),
+            "Cuerpo": response.css(
+                "div.entry-content div.content-inner p ::text"
+            ).getall(),
+            "Tags": response.css(
+                "div.entry-content div.content-inner div.jeg_post_tags a::text"
+            ).getall(),
+            "Imajes": [
+                img.attrib["data-src"]
+                for img in response.css("div.jeg_inner_content img")
+            ],
+        }
