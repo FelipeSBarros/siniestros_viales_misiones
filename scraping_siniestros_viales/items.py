cd3d51fdf2c2de803a9973c9be4d9e8d1770ed79
Felipe Sodré M. Barros <felipe.b4rros@gmail.com>
2023-10-27 15:55:50 -0300

    corrige codestyle

--- scraping_siniestros_viales/items.py
+++ scraping_siniestros_viales/items.py
@@ -1,19 +1,26 @@
 import scrapy
-from itemloaders.processors import MapCompose, Join, TakeFirst
+from itemloaders.processors import Join, MapCompose, TakeFirst
 
 
 class NewsItem(scrapy.Item):
-    titulo = scrapy.Field(input_processor=MapCompose(str.strip),
-                                output_processor=TakeFirst())
-    subtitulo = scrapy.Field(input_processor=MapCompose(str.strip),
-                                output_processor=TakeFirst())
-    url = scrapy.Field(input_processor=MapCompose(str.strip),
-                                output_processor=TakeFirst())
-    fecha = scrapy.Field(input_processor=MapCompose(str.strip),
-                                output_processor=TakeFirst())
-    cuerpo = scrapy.Field(input_processor=MapCompose(str.strip),
-                          output_processor=Join())
-    tags = scrapy.Field(input_processor=MapCompose(str.strip),
-                        output_processor=Join(","))
-    url_imagenes = scrapy.Field(input_processor=MapCompose(str.strip),
-                                output_processor=TakeFirst())
+    titulo = scrapy.Field(
+        input_processor=MapCompose(str.strip), output_processor=TakeFirst()
+    )
+    subtitulo = scrapy.Field(
+        input_processor=MapCompose(str.strip), output_processor=TakeFirst()
+    )
+    url = scrapy.Field(
+        input_processor=MapCompose(str.strip), output_processor=TakeFirst()
+    )
+    fecha = scrapy.Field(
+        input_processor=MapCompose(str.strip), output_processor=TakeFirst()
+    )
+    cuerpo = scrapy.Field(
+        input_processor=MapCompose(str.strip), output_processor=Join()
+    )
+    tags = scrapy.Field(
+        input_processor=MapCompose(str.strip), output_processor=Join(",")
+    )
+    url_imagenes = scrapy.Field(
+        input_processor=MapCompose(str.strip), output_processor=TakeFirst()
+    )
