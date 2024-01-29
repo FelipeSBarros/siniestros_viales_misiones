24fc1f1682b92554d6b2bd478dbd44edbddb6dcd
Felipe Sodré <felipe.b4rros@gmail.com>
2024-01-28 22:16:45 -0300

    add fake agent user

--- scraping_siniestros_viales/settings.py
+++ scraping_siniestros_viales/settings.py
@@ -15,9 +15,15 @@ NEWSPIDER_MODULE = "scraping_siniestros_viales.spiders"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = "scraping_siniestros_viales (+http://www.yourdomain.com)"
+FAKEUSERAGENT_PROVIDERS = [
+    'scrapy_fake_useragent.providers.FakeUserAgentProvider',  # This is the first provider we'll try
+    'scrapy_fake_useragent.providers.FakerProvider',  # If FakeUserAgentProvider fails, we'll use faker to generate a user-agent string for us
+    'scrapy_fake_useragent.providers.FixedUserAgentProvider',  # Fall back to USER_AGENT value
+]
+USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
@@ -50,9 +56,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    "scraping_siniestros_viales.middlewares.ScrapingSiniestrosVialesDownloaderMiddleware": 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+   "scraping_siniestros_viales.middlewares.ScrapingSiniestrosVialesDownloaderMiddleware": 543,
+   'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+   'scrapy_fake_useragent.middleware.RetryUserAgentMiddleware': 401,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
